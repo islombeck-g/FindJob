6a928fd7a58219cd3fd921006c3edd63e13eb54d
Islombek <whoisit864@gmail.com>
2023-10-05 22:24:35 +0300

    hello view is ready

---
+++ newProject/EnterModule/EnterMainView.swift
@@ -0,0 +1,67 @@
+import SwiftUI
+
+struct EnterMainView: View {
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                
+                Color.black
+                    .ignoresSafeArea()
+                
+                VStack{
+                    Group {
+                        Text ("Здравствуйте!")
+                            .font(.system(size: 28))
+                            .fontWeight(.bold)
+                        Text ("Биржа талантов - возможность получить любимую работу")
+                            .font(.system(size: 20))
+                    }
+                    .foregroundStyle(.white)
+                    .padding(.horizontal)
+                    .multilineTextAlignment(.center)
+                    
+                    Group{
+                        NavigationLink{
+                            //go to enter view
+                        }label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 8)
+                                    .frame(width: UIScreen.main.bounds.width - 32, height: 46)
+                                    .foregroundColor(.white)
+                                Text ("Вход")
+                                    .foregroundColor(.black)
+                                    .fontWeight(.bold)
+                            }
+                        }
+                        //                    .frame(width: UIScreen.main.bounds.width - 32)
+                        //                    .padding(.vertical, 9)
+                        //                    .background(Color.white)
+                        
+                        
+                        NavigationLink{
+                            //go to registration view
+                        }label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.white, lineWidth: 2)
+                                    .frame(width: UIScreen.main.bounds.width - 32, height: 46)
+                                    .foregroundColor(.black)
+                                Text("Регистрация")
+                                    .foregroundColor(.white)
+                                    .fontWeight(.bold)
+                            }
+                        }
+                        
+                    }
+                    
+                    
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    EnterMainView()
+}
---
+++ newProject/newProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  newProjectApp.swift
+//  newProject
+//
+//  Created by Islombek Gofurov on 05.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct newProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EnterMainView()
+        }
+    }
+}
