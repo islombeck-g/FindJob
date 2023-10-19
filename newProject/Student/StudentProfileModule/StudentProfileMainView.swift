129f826442eda7a1b431f3785fca20f7b27edbb2
Islombek <whoisit864@gmail.com>
2023-10-19 09:12:21 +0300

    toolbar
    is ready

--- newProject/Student/StudentProfileModule/StudentProfileMainView.swift
+++ newProject/Student/StudentProfileModule/StudentProfileMainView.swift
@@ -1,8 +1,57 @@
 import SwiftUI
 
 struct StudentProfileMainView: View {
+    
+    @State private var isFavorite = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            ZStack{
+                Color.white.ignoresSafeArea()
+                
+                VStack {
+                    HStack {
+                        Button{
+                            //don't understand why it is here
+                        }label: {
+                            Image(systemName: "bell")
+                                .padding(5)
+                        }
+                        .padding(.leading, 7)
+                        
+                        Spacer()
+                        
+                        Text("Профиль")
+                            
+                        Spacer()
+                        
+                        Menu {
+                            Button {
+                                
+                            }label:{
+                                Text("Редактировать профиль")
+                            }
+                            Divider()
+                            Button(role: .destructive) {
+                                
+                            }label:{
+                                Text("Удалить аккаунт")
+                            }
+                        }label: {
+                            Image(systemName: "ellipsis")
+                                .rotationEffect(.degrees(90))
+                                .frame(width: 20, height: 30)
+                                .padding(4)
+                                .padding(.trailing, 6)
+                        }
+                    }
+                    .font(.system(size: 18))
+                    Text("some")
+                    Spacer()
+                }
+            }
+        }
+        
     }
 }
 
