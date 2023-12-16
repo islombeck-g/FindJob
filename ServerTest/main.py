c8ebb644c37693b05b1c8ee886263a834b2aa160
Islombek <whoisit864@gmail.com>
2023-12-16 23:20:01 +0300

    update to normal auth code
    it is works)

--- ServerTest/main.py
+++ ServerTest/main.py
@@ -10,43 +10,99 @@ app = Flask(__name__)
 db = pymysql.connect(host=host_const, user=user_const, password=password_const, database=name_const)
 cursor = db.cursor()
 
-# Эндпоинт для регистрации пользователя
-@app.route('/register', methods=['POST'])
+# Эндпоинт для регистрации нового пользователя
+@app.route('/registration', methods=['POST'])
 def register():
-    data = request.get_json()
+    try:
 
-    username = data.get('username')
-    password = data.get('password')
+        data = request.get_json()
 
-    if not username or not password:
-        return jsonify({'error': 'Missing username or password'}), 400
+        # Получение данных из запроса
+        first_name = data.get('firstName')
+        second_name = data.get('secondName')
+        patronymic_name = data.get('patronymicName')
+        gender = data.get('gender')
+        city = data.get('city')
+        university = data.get('university')
+        phone_number = data.get('phoneNumber')
+        image = data.get('image')
+        email = data.get('email')
+        password = data.get('password')
+        birth_date = data.get('birthDate')
+        about_me = data.get('aboutMe')
+        institute = data.get('institute')
+        direction = data.get('direction')
+        login = data.get('login')
+        # Хеширование пароля
 
-    hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
-    print(hashed_password)
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
 
-    cursor.execute('INSERT INTO users (username, password) VALUES (%s, %s)', (username, hashed_password))
-    db.commit()
+        # Вставка нового пользователя в базу данных
+        cursor.execute(
+            'INSERT INTO users (firstName, secondName, patronymicName, gender, city, university, phoneNumber, '
+            'image, email, password, birthDate, aboutMe, institute, direction, login) VALUES (%s, %s, %s, %s, %s, %s, %s, '
+            '%s, %s, %s, %s, %s, %s, %s, %s)',
+            (first_name, second_name, patronymic_name, gender, city, university, phone_number, image, email,
+             hashed_password, birth_date, about_me, institute, direction, login)
+        )
 
-    return jsonify({'message': 'User registered successfully'})
+        db.commit()
+
+        return jsonify({'message': 'Registration successful'})
+
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        return jsonify({'error': f'Server error: {str(e)}'}), 500
 # Эндпоинт для авторизации пользователя
 @app.route('/login', methods=['POST'])
 def login():
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    username = data.get('username')
-    password = data.get('password')
+        userLogin = data.get('userLogin')
+        password = data.get('password')
 
-    # Retrieve hashed password from the database based on the username
-    cursor.execute('SELECT password FROM users WHERE username = %s', (username,))
-    result = cursor.fetchone()
+        # Retrieve hashed password from the database based on the username
+        cursor.execute('SELECT password FROM users WHERE login = %s', (userLogin,))
+        result = cursor.fetchone()
 
-    if result is None:
-        return jsonify({'error': 'User not found'}), 401
+        # Retrieve hashed password from the database based on the username
+        cursor.execute(
+            'SELECT password, firstName, secondName, patronymicName, gender, city, university, phoneNumber, image, email, birthDate, aboutMe, institute, direction, login FROM users WHERE login = %s',
+            (userLogin,))
+        result = cursor.fetchone()
 
-    stored_password = result[0]
-    if not check_password_hash(stored_password, password):
-        return jsonify({'error': 'Invalid password'}), 401
-    return jsonify({'message': 'Login successful'})
+        if result is None:
+            return jsonify({'error': 'User not found'}), 401
+
+        stored_password = result[0]
+        if not check_password_hash(stored_password, password):
+            return jsonify({'error': 'Invalid password'}), 401
+
+        # Extract user data from the result
+        user_data = {
+            'firstName': result[1],
+            'secondName': result[2],
+            'patronymicName': result[3],
+            'gender': result[4],
+            'city': result[5],
+            'university': result[6],
+            'phoneNumber': result[7],
+            'image': result[8],
+            'email': result[9],
+            'birthDate': result[10],
+            'aboutMe': result[11],
+            'institute': result[12],
+            'direction': result[13],
+            'login': result[14]
+        }
+        return jsonify({'message': 'Login successful', 'userData': user_data})
+
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        return jsonify({'error': f'Server error: {str(e)}'}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4568)
